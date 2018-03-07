518d5dd12b049fbb8687f2550b2a44429fea8faa
linchunxiao <1917484@qq.com>
2018-03-08 01:13:02 +0800

    lin
    C func of runtime

--- RunLoopLin/AppDelegate.h
+++ RunLoopLin/AppDelegate.h
@@ -6,6 +6,9 @@
 //  Copyright © 2018年 buTing. All rights reserved.
 //
 
+// runtime为：objc_msgSend( id self, @selecto(method), @"Para")
+// C为 void method(id self, SEL _CMD, id objc)
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
