8eb876ec0c61bb8698af10f0c31b10b21efc9810
linchunxiao <1917484@qq.com>
2018-03-20 06:46:46 +0800

    "this is an demo ci for id_lin"

---
+++ RunLoopLin/IDTypeLin.m
@@ -0,0 +1,14 @@
+//
+//  IDTypeLin.m
+//  RunLoopLin
+//
+//  Created by cxlin18 on 2018/3/20.
+//  Copyright © 2018年 buTing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+typedef struct objc_object_lin {
+    Class isa;
+} *id_lin;
+
+
